5046dfae3b31c155152c8da17171461309f61de4
GUSTAVO KNEVITZ KLEIN WITT <05074308089@senacrs.edu.br>
2026-03-23 14:36:52 -0300

    subindo os arquivos para o GitHub

---
+++ Varieveis/exercicio1.js
@@ -0,0 +1,11 @@
+const Nome = "Gustavo"
+const sobrenome = "Witt"
+let Idade = 16
+let statusDeEstudante = true
+
+//console.log(Nome)
+//console.log(sobrenome)
+//console.log(Idade)
+//console.log(statusDeEstudante)
+
+console.log("olá meu nome é " + Nome + " e eu tenho " + Idade + " anos de idade!")
---
+++ Varieveis/exercicio3.js
@@ -0,0 +1,12 @@
+const nomeDaNave = "Comando Estelar"
+let NTripulantes = 6
+let destino = "Marte"
+let tempoEst = 10
+const misaoLancada = true
+
+//console.log(nomeDaNave, NTripulantes, destino, tempoEst, misaoLancada)
+console.log(nomeDaNave)
+console.log(NTripulantes)
+console.log(destino)
+console.log(tempoEst)
+console.log(misaoLancada)
